1ba905fe32ab0fd129f27ce5f53db88de531ed5f
Allan <allangdc@gmail.com>
2014-08-19 20:26:46 -0300

    Added Soma_Aritmetica

--- pdi_01.m
+++ pdi_01.m
@@ -2,37 +2,22 @@
 
 XSIZE=1300;
 YSIZE=900;
-IMG = "/home/allan/Pictures/tigre.jpg";
+%IMG = "/home/allan/Pictures/tigre.jpg";
+IMG = "/home/allan/Pictures/rostos/A01.jpg";
 
 pkg load image
 
-img = imread(IMG);
+img = im2double( imread(IMG) );
 [lin, col, ch] = size(img);
 
 r = img(:,:,1);
 g = img(:,:,2);
 b = img(:,:,3);
-ar = cast(r, 'double');
-ag = cast(g, 'double');
-ab = cast(b, 'double');
-media_aritmetica = cast((ar + ag + ab)/3, 'uint8');
-soma_simetrica = zeros(lin, col);
-for l=1:lin
-    for c=1:col
-        pa = ar(l, c);
-        pb = ag(l, c);
-        pc = ab(l, c);
-        den = (1-pa-pb-pc+ pa*pb + pa*pc + pb*pc);
-        num=(pa*pb*pc);
-        if(den~=0)
-            ma = num/den;
-        else
-            ma = num;
-        endif
-        soma_simetrica(l, c) = cast(ma, 'uint8');
-    endfor
-endfor
-
+ar = r;
+ag = g;
+ab = b;
+media_aritmetica = ( (ar + ag + ab)./ 3 );
+soma_simetrica = (ar .* ag .* ab) ./ (1-ar-ag-ab+(ar.*ag)+(ar.*ab)+(ag.*ab));
 rgb_to_gray = rgb2gray(img);
 
 
@@ -71,10 +56,5 @@ subplot(1,2,1)
 imshow(soma_simetrica);
 subplot(1,2,2)
 hist(soma_simetrica(:), 32);
-
-r(150,150)
-g(150,150)
-b(150,150)
-soma_simetrica(150,150)
 
 pause;
